6fc7a2fb8b29c0ad24ffe7aa12ebe969fd3b8844
Munjogu123 <fredmunjogu@gmail.com>
2024-01-05 13:18:23 +0300

    creates a copy of a list and modifies copy

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    listCopy = my_list.copy()
+    length = len(my_list)
+
+    if (idx < 0):
+        return listCopy
+
+    if (idx >= length):
+        return listCopy
+
+    for i in range(length):
+        if (i == idx):
+            listCopy[i] = element
+    
+    return listCopy

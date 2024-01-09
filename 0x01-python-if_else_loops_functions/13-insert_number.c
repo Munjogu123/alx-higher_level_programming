734e8f1409ef12a98b9ed940097a38e8ab1ff516
Munjogu123 <fredmunjogu@gmail.com>
2024-01-09 17:01:35 +0300

    inserts no to a sorted list

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -19,6 +19,7 @@ listint_t *insert_node(listint_t **head, int number)
 		return (NULL);
 
 	new->n = number;
+	new->next = NULL;
 
 	if (*head == NULL || (*head)->n >= number)
 	{
@@ -27,7 +28,7 @@ listint_t *insert_node(listint_t **head, int number)
 		return (new);
 	}
 
-	while (current->n < number)
+	while (current && current->n < number)
 	{
 		current = current->next;
 		count++;

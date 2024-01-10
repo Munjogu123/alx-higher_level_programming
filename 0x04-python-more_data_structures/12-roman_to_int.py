3b0b2874791c680b6f6a03c33e94e795f27aba5c
Munjogu123 <fredmunjogu@gmail.com>
2024-01-10 21:18:13 +0300

    converts roman numeral to integer

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -4,7 +4,7 @@ def roman_to_int(roman_string):
     res = 0
     length = len(roman_string)
 
-    if roman_string is None:
+    if not roman_string or type(roman_string) != str:
         return (0)
 
     for i in range(length):

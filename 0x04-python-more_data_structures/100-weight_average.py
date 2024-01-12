b7fdcd66269a54cfe07f05b724739acdafd1ff2b
Munjogu123 <fredmunjogu@gmail.com>
2024-01-12 23:19:59 +0300

    returns weighted average

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    product = 1
+    weight = 0
+    sum = 0
+
+    if len(my_list) == 0:
+        return 0
+
+    for i in my_list:
+        product = i[0] * i[1]
+        weight += i[1]
+        sum += product
+    
+    return (sum / weight)

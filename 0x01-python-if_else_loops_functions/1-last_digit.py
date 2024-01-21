acbc907dd7306c2656141beea1f7c440229ef77f
Munjogu123 <fredmunjogu@gmail.com>
2024-01-21 17:34:28 +0300

    finds last digit of a no

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,14 +1,21 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-last = abs(number) % 10
-if (last < 0):
-    last = -last
-print(f'Last digit of {number} is {last} and is ', end='')
-
-if last > 5:
-    print('greater than 5')
-elif last == 0:
-    print('0')
+if (number < 0):
+    num = -(number)
 else:
-    print('less than 6 and not 0')
+    num = number
+
+last = num % 10
+if (number < 0):
+    last = -(last)
+else:
+    last = last
+
+if (last > 5):
+    print("Last digit of {} is {} and is greater than 5".format(number, last))
+elif (last == 0):
+    print("Last digit of {} is {} and is 0".format(number, last))
+elif (last < 6 and last != 0):
+    print("Last digit of {} is {} and is less than 6 and not 0"
+          .format(number, last))

7069144b01bd636f9c65baa462fd398ea09942c7
Munjogu123 <fredmunjogu@gmail.com>
2024-01-23 23:20:02 +0300

    prints square preceeding with space

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -43,11 +43,16 @@ class Square:
 
     @position.setter
     def position(self, value):
-        if type(value[0]) is not int or type(value[1] is not int):
+        if type(value) is not tuple:
             raise TypeError("position must be a tuple of 2 positive integers")
-        if type(value) is not tuple or value[0] < 0 or value[1] < 0:
+        elif len(value) != 2:
             raise TypeError("position must be a tuple of 2 positive integers")
-        self.__position = value
+        elif type(value[0]) is not int or type(value[1]) is not int:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif value[0] < 0 or value[0] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        else:
+            self.__position = value
 
     """ Calculates the area of a square
 

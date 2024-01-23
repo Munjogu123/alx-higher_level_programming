696a19f2ebe5120cf12cc94de8312717144215d1
Munjogu123 <fredmunjogu@gmail.com>
2024-01-23 23:28:53 +0300

    prints square preceeding with space

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -76,7 +76,7 @@ class Square:
             print()
         else:
             for i in range(self.__position[1]):
-                    print("")
+                print("")
             for i in range(self.__size):
                 print(" " * self.__position[0], end="")
                 print("#" * self.__size)

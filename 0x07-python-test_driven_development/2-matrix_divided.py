107dcf3f418e342f85bc3b1fd5527423ef39d1ba
Munjogu123 <fredmunjogu@gmail.com>
2024-01-25 22:03:04 +0300

    divides a matric by div variable

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,5 +1,27 @@
 #!/usr/bin/python3
+"""
+
+This module contains a function that divides a matrix by div variable
+
+"""
+
+
 def matrix_divided(matrix, div):
+    """ Divides matrix by div variable
+
+    Args:
+        matrix: 2d list to be divided
+        div: the divisor
+
+    Raises:
+        TypeError: if div is not an int or float
+        if matrix is not a list of lists
+        if length of the rows in a matrix vary
+        ZeroDivisionError: if div is equal to 0
+
+    Returns:
+        a new matrix containing the quotient of the operation
+    """
     if type(div) not in [int, float]:
         raise TypeError("div must be a number")
     if div == 0:
@@ -7,9 +29,10 @@ def matrix_divided(matrix, div):
 
     for i in range(len(matrix) - 1):
         if type(matrix) is not list or type(matrix[i]) is not list:
-            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+            raise TypeError
+        ("matrix must be a matrix (list of lists) of integers/floats")
         if len(matrix[i + 1]) != len(matrix[i]):
             raise TypeError("Each row of the matrix must have the same size")
-            
+
     result = [list(map(lambda i: round(i / div, 2), row)) for row in matrix]
     return result

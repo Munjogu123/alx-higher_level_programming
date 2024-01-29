702974f41cfcf59b8bc9995ea601e577dd38f0a4
Munjogu123 <fredmunjogu@gmail.com>
2024-01-29 17:13:14 +0300

    adds height and width attributes

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -24,12 +24,13 @@ class Rectangle:
 
     @width.setter
     def width(self, value):
+        """ sets width value """
         if type(value) is not int:
             raise TypeError("width must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("width must be >= 0")
-        else:
-            self.__width = value
+
+        self.__width = value
 
     @property
     def height(self):
@@ -42,9 +43,10 @@ class Rectangle:
 
     @height.setter
     def height(self, value):
+        """ sets height value """
         if type(value) is not int:
             raise TypeError("height must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("height must be >= 0")
-        else:
-            self.__height = value
+
+        self.__height = value

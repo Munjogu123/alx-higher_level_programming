d08dfb2ce6d8c31b5ae8dbb10b2091f5823b16d0
Munjogu123 <fredmunjogu@gmail.com>
2024-01-29 10:43:06 +0300

    gets the value of a rectangle

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+""" This module defines a rectangle """
+
+
+class Rectangle:
+    def __init__(self, width=0, height=0):
+        """ Initializes a Rectangle
+
+        Args:
+            width: width of the rectangle
+            height: height of the rectangle
+        """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """ Retrieves the width of the rectangle
+
+        Has a setter method that sets the value and raises errors
+        if certain conditions are not met
+        """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        """ Retrieves the height of the rectangle
+
+        Has a setter method that sets the value and raises errors
+        if certain conditions are not met
+        """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value

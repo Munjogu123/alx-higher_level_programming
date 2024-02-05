be5ffea4e403e8ff97c287e82e65a21cfd7616a3
Munjogu123 <fredmunjogu@gmail.com>
2024-02-05 19:35:02 +0300

    adds new class square

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" Inherits from Rectangle and BaseGeometry """
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+""" This module defines a square """
+
+
+class Square(Rectangle):
+    def __init__(self, size):
+        """ Initializes size and checks if it is
+        a positive integer """
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        """ Implements area of a square """
+        return self.__size ** 2
+
+    def __str__(self):
+        """ Returns the description of the rectangle """
+        return f"[Rectangle] {self.__size}/{self.__size}"

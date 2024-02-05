ded37353aaa6d4e53508b09aa1652cbf6a170ceb
Munjogu123 <fredmunjogu@gmail.com>
2024-02-05 21:23:27 +0300

    changes how the operator works

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -5,9 +5,23 @@
 class MyInt(int):
     """ defines how the operators work """
     def __eq__(self, __value: object) -> bool:
-        """ Changes how the == operator works """
+        """ Changes how the == operator works
+
+        Args:
+            value: refers to the objects being compared
+
+        Returns:
+            True if the numbers are not equal otherwise False
+        """
         return not(super().__eq__(__value))
 
     def __ne__(self, __value: object) -> bool:
-        """ Changes how the != operator works """
+        """ Changes how the != operator works
+
+        Args:
+            value: refers to the objects being compared
+
+        Returns:
+            False if the numbers are not equal otherwise True
+        """
         return not(super().__ne__(__value))

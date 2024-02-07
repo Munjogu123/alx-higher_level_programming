acc6f9ec3227f400b642e7c12da9a9745b4adf9a
Munjogu123 <fredmunjogu@gmail.com>
2024-02-07 10:21:25 +0300

    returns the dictionary description with simple data structure

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -7,11 +7,11 @@ description with simple data structure
 
 def class_to_json(obj):
     """ returns the dictionary description with simple data structure
- 
+
     Args:
         obj: obj to be encoded
 
     Returns:
-        returns the dictionary description with simple data structure 
+        returns the dictionary description with simple data structure
     """
     return obj.__dict__

fb88c9e425ca0e7c6383b8157d137df0aee78534
Munjogu123 <fredmunjogu@gmail.com>
2024-04-09 18:46:34 +0300

    adds print method

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,20 +1,15 @@
 #!/usr/bin/node
 
-const Rectangle = require('./4-rectangle');
+const prevSquare = require('./5-square');
 
-class Square extends Rectangle {
-  constructor (size) {
-    super(size, size);
-    this.size = size;
-  }
-
+class Square extends prevSquare {
   charPrint (c) {
     if (c === undefined) {
       c = 'X';
     }
 
-    for (let i = 0; i < this.size; i++) {
-      console.log(c.repeat(this.size));
+    for (let i = 0; i < this.height; i++) {
+      console.log(c.repeat(this.width));
     }
   }
 }

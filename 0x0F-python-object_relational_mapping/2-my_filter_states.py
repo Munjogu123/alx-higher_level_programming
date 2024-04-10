55f206e7165e15bec62030faf09dc509e70ce540
Munjogu123 <fredmunjogu@gmail.com>
2024-04-10 15:47:19 +0300

    displays all values in the states table of hbtn_0e_0_usa where name matches the argument

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" Performs SQL queries in python script """
+import MySQLdb
+from sys import argv
+
+if __name__ == '__main__':
+    """displays all values in the states table of
+    hbtn_0e_0_usa where name matches the argument"""
+    db = MySQLdb.connect(host="localhost",
+                         user=argv[1], passwd=argv[2], db=argv[3])
+    
+    cur = db.cursor()
+    cur.execute(f'SELECT * FROM states WHERE name = "{argv[4]}"')
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)

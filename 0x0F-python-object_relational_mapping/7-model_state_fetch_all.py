50178b47a68e8b9f253cea91b1a0b15951c9d529
Munjogu123 <fredmunjogu@gmail.com>
2024-04-11 14:26:11 +0300

    lists all State objects from the database hbtn_0e_6_usa

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sys import argv
+
+if __name__ == '__main__':
+    """ lists all State objects from the database hbtn_0e_6_usa """
+    db_url = f'mysql+mysqldb://{argv[1]}:{argv[2]}@localhost/{argv[3]}'
+    engine = create_engine(db_url)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    for value in session.query(State).order_by(State.id):
+        print(f'{value.id}: {value.name}')

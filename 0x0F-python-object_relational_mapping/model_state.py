8f19b17b24818c6f7c6eeea0f2a5261269355126
Munjogu123 <fredmunjogu@gmail.com>
2024-04-11 14:01:44 +0300

    contains the class definition of a State

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -5,11 +5,13 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
+
 class State(Base):
     """
     links to MySQL table states
     inherits from Base
     """
     __tablename__ = 'states'
-    id = Column(Integer, autoincrement=True, unique=True, nullable=False, primary_key=True)
+    id = Column(Integer, autoincrement=True, unique=True,
+                nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)

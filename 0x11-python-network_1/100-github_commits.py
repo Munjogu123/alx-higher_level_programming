6ed9c428edd864abf20f6efa58e448eba6f39b01
Munjogu123 <fredmunjogu@gmail.com>
2024-04-26 18:27:18 +0300

    lists 10 commits from a repository

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -13,10 +13,6 @@ if __name__ == '__main__':
     response = requests.get(url)
     commits = response.json()
 
-    try:
-        for i in range(10):
-            print("{}: {}".format(
-                commits[i].get('sha'),
-                commits[i].get('commit').get('author').get('name')))
-    except IndexError:
-        pass
+    for commit in commits[:10]:
+        print(commit.get('sha'), end=': ')
+        print(commit.get('commit').get('author').get('name'))

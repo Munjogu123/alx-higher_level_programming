e9083d4ddb254e10361a2cf2c1bb290385e8c612
Munjogu123 <fredmunjogu@gmail.com>
2024-04-26 17:48:09 +0300

    takes in a letter and sends a POST request to 'http://0.0.0.0:5000/search_user'

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -13,7 +13,7 @@ if __name__ == '__main__':
         letter = sys.argv[1]
 
     payload = {'q': letter}
-    r = requests.post('http://0.0.0.0:5000/search_user', params=payload)
+    r = requests.post('http://0.0.0.0:5000/search_user', data=payload)
 
     try:
         response = r.json()

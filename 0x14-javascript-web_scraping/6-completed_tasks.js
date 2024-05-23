a140b8ca6704974a5df2ed01a139d3a85ef22c97
Munjogu123 <fredmunjogu@gmail.com>
2024-05-23 23:55:40 +0300

    computes the number of tasks completed by user id

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -4,16 +4,20 @@ const request = require('request');
 const url = process.argv[2];
 
 request(url, (error, response, body) => {
-  if (!error) {
-    const contents = JSON.parse(body);
-    const completed = {};
-    contents.forEach((content) => {
-      if (content.completed && completed[content.userId] === undefined) {
-        completed[userId] = 1;
-      } else if (content.completed) {
-        completed[content.userId] += 1;
-      }
-    });
-    console.log(completed);
+  if (error) {
+    console.log(error);
+    return;
   }
+
+  const completed = {};
+  body.forEach((todo) => {
+    if (todo.completed) {
+      if (!completed[todo.userId]) {
+        completed[todo.userId] = 1;
+      } else {
+        completed[todo.userId] += 1;
+      }
+    }
+  });
+  console.log(completed);
 });
